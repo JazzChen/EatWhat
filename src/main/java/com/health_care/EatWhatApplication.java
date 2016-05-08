2dcc5408bd9e5eb12362bcd2c2e378de5f688954
Jazz Chen <jazz.chen@wolaidai.com>
2016-05-08 14:02:17 +0800

    initial a spring boot project

---
+++ src/main/java/com/health_care/EatWhatApplication.java
@@ -0,0 +1,12 @@
+package com.health_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatWhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatWhatApplication.class, args);
+	}
+}
