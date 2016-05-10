5c064b83f344274b681414b9f86e00ac66db6949
Jazz Chen <jazz.chen@wolaidai.com>
2016-05-10 18:52:16 +0800

    change to generate war package

--- src/main/java/com/health_care/EatWhatApplication.java
+++ src/main/java/com/health_care/EatWhatApplication.java
@@ -2,9 +2,16 @@ package com.health_care;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EatWhatApplication {
+public class EatWhatApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EatWhatApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EatWhatApplication.class, args);
