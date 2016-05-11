d2f998bbe0359037f866547dbf1a4adc74ea11ac
Jazz Chen <jazz.chen@wolaidai.com>
2016-05-11 10:07:27 +0800

    convenient develop mode: as a runable app embeded jetty or tomcat

--- src/main/java/com/health_care/EatWhatApplication.java
+++ src/main/java/com/health_care/EatWhatApplication.java
@@ -2,17 +2,10 @@ package com.health_care;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EatWhatApplication extends SpringBootServletInitializer {
+public class EatWhatApplication {
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(EatWhatApplication.class);
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(EatWhatApplication.class, args);
 	}
